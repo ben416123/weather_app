a3ca67b1e49b1116b5964150aebe1a619e94999b
ben416123 <c3045540@newcastle.ac.uk>
2024-07-28 18:16:57 +0100

    set unit action added

--- weatherapp/core/__init__.py
+++ weatherapp/core/__init__.py
@@ -3,3 +3,4 @@ from .forecast import Forecast
 from .unit_converter import UnitConverter
 from forecast_type import ForecastType
 from .request import Request
+from .set_unit_action import SetUnitAction
