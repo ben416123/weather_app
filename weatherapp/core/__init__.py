60da6adb0f6fb4f5124b26ca0b832fe7e4c7165e
ben416123 <c3045540@newcastle.ac.uk>
2024-07-28 20:32:27 +0100

    corrected imports

--- weatherapp/core/__init__.py
+++ weatherapp/core/__init__.py
@@ -1,6 +1,6 @@
 from .base_enum import BaseEnum
 from .forecast import Forecast
 from .unit_converter import UnitConverter
-from forecast_type import ForecastType
+from .forecast_type import ForecastType
 from .request import Request
 from .set_unit_action import SetUnitAction
